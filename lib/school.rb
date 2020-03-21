c22f464cb49c70e3b4c591e9abd44c1bb80cb896
Craig Storer <craigstorer@gmail.com>
2020-03-21 20:21:35 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
-# code here!
+class School 
+  
+  def initialize(model)
+    @model = model 
+    @hash = {}
+  end
+  
+  def roster
+    @hash = {}
+  end
+  
+  def add_student=(name, grade)
+    @name = name
+    @grade= num
+  end    
+  
+  def add_student
+    @add_student
+  end
+  
+end
